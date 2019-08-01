11587559ccc55497a679daee9b08260bef7cf60d
林东吴 <lindongwu11@gmail.com>
2019-08-02 00:46:35 +0800

    feat: controlled natural language interface to search tag

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { Select } from 'antd';
+
 import ClickableText from './ClickableText';
 import Tag from './Tag';
 
@@ -21,7 +23,7 @@ const Wrapper = styled.div`
   margin: auto;
   padding: 6px;
   width: 612px;
-  border: 2px ridge #5C0D12;
+  border: 2px ridge #5c0d12;
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
@@ -36,35 +38,54 @@ const TagWrapper = styled.div`
 
 const InputWrapper = styled.div`
   display: flex;
+  align-items: center;
   margin-top: 2px;
 `;
 
-const Input = styled.input`
-  padding: 0 3px;
-  width: 310px;
+const Input = styled(Select)`
+  padding: 0;
+  width: 310px !important;
   line-height: 20px;
-  border: 1px solid #B5A4A4;
+  border: 1px solid #b5a4a4;
   border-radius: 3px;
-  background: #EDEADA;
+  background: #edeada;
   &:hover {
-    background: #F3F0E0;
+    background: #f3f0e0;
   }
   &::placeholder {
-    color:#9F746F;
+    color: #9f746f;
+  }
+
+  & .ant-select-selection {
+    padding: 0;
+    margin: 0;
+    border: none;
+    background: none;
+  }
+
+  & .ant-select-selection__choice,
+  .ant-select-selection__choice__remove,
+  .ant-select-selection__choice__remove:hover {
+    background-color: #666;
+    color: white;
+    &:hover {
+      opacity: 0.8;
+    }
   }
 `;
 
 const Button = styled.button`
   margin: 0 2px;
   font-size: 12px;
-  color: #5C0D12;
+  color: #5c0d12;
   line-height: 24px;
-  border: 2px solid #B5A4A4;
+  height: 34px;
+  border: 2px solid #b5a4a4;
   border-radius: 3px;
-  background: #EDEADA;
+  background: #edeada;
   &:hover {
     border-color: #977273;
-    background: #F3F0E0;
+    background: #f3f0e0;
   }
 `;
 
@@ -73,23 +94,57 @@ const ActionWrapper = styled.div`
   margin-top: 4px;
 `;
 
-const SearchBox = () => (
-  <Wrapper>
-    <TagWrapper>
-      {tagList.map(type => (<Tag type={type} />))}
-    </TagWrapper>
+export default function SearchBox() {
+  const [tags, setTags] = useState([]);
+  useEffect(() => {
+    // TODO: fetch this from server
+    const fakeDataFromServer = {
+      language: ['english'],
+      character: ['tomoko kuroki'],
+      femail: ['beauty mark', 'bikini', 'dark skin', 'anal'],
+    };
+    setTags(
+      Object.keys(fakeDataFromServer).flatMap(tagCategory =>
+        fakeDataFromServer[tagCategory].map(subTag => ({ value: `${tagCategory}:${subTag}`, label: subTag })),
+      ),
+    );
+  }, []);
+  return (
+    <Wrapper>
+      <TagWrapper>
+        {tagList.map(type => (
+          <Tag type={type} />
+        ))}
+      </TagWrapper>
 
-    <InputWrapper>
-      <Input placeholder="Search Keywords" />
-      <Button>Apply Filter</Button>
-      <Button>Clear Filter</Button>
-    </InputWrapper>
+      <InputWrapper>
+        <Input
+          mode="tags"
+          style={{ width: '100%' }}
+          placeholder="select tags"
+          onChange={value => {
+            console.log(`selected ${value}`);
+          }}
+          optionLabelProp="label"
+        >
+          {tags.map(({ value, label }) => (
+            <Select.Option value={value} label={label}>
+              {value}
+            </Select.Option>
+          ))}
+        </Input>
+        <Button>Apply Filter</Button>
+        <Button>Clear Filter</Button>
+      </InputWrapper>
 
-    <ActionWrapper>
-      <ClickableText fontSize={10} underline>Show Advanced Options</ClickableText>
-      <ClickableText fontSize={10} underline style={{ marginLeft: '12px' }}>Show File Search</ClickableText>
-    </ActionWrapper>
-  </Wrapper>
-);
-
-export default SearchBox;
+      <ActionWrapper>
+        <ClickableText fontSize={10} underline>
+          Show Advanced Options
+        </ClickableText>
+        <ClickableText fontSize={10} underline style={{ marginLeft: '12px' }}>
+          Show File Search
+        </ClickableText>
+      </ActionWrapper>
+    </Wrapper>
+  );
+}
